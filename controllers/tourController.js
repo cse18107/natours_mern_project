503ad55c1ad8f6f282904e6825de4e7619556e6c
cse18107 <cse18107@cemk.ac.in>
2021-08-19 15:58:21 +0530

    adding change

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,77 +1,58 @@
-const fs=require('fs');
+const fs = require('fs');
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).json({
+      status: 'Not found',
+      message: 'tour Not found',
+    });
+  }
+};
 
 exports.getAllTours = (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      requestTime: req.requestTime,
-      data: {
-        tours,
-      },
-    });
-  };
- exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find((ele) => ele.id === id);
-    if (tour) {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour: tour,
-        },
-      });
-    } else {
-      res.status(404).json({
-        status: 'not valid',
-        message: 'tour not found',
-      });
+  res.status(200).json({
+    status: 'success',
+    requestTime: req.requestTime,
+    data: {
+      tours,
+    },
+  });
+};
+exports.getTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+};
+exports.postTour = (req, res) => {
+  console.log(req.body);
+  const tour = Object.assign({ id: tours.length }, req.body);
+  tours.push(tour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      console.log('something wrong happens');
     }
-  };
-  exports.postTour = (req, res) => {
-    console.log(req.body);
-    const tour = Object.assign({ id: tours.length }, req.body);
-    tours.push(tour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        console.log('something wrong happens');
-      }
-    );
-    res.send('done');
-  };
-  exports.patchTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find((ele) => ele.id === id);
-    if (tour) {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: tour,
-        },
-      });
-    } else {
-      res.json({
-        message: 'URL not currect',
-      });
-    }
-  };
-  exports.deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find((el) => el.id === id);
-    if (tour) {
-      res.status(202).json({
-        status: 'success',
-        message: 'data is deleted',
-      });
-    } else {
-      res.status(404).json({
-        status: 'unsuccess',
-        message: 'data is not found',
-      });
-    }
-  };
+  );
+  res.send('done');
+};
+exports.patchTour = (req, res) => {
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+};
+exports.deleteTour = (req, res) => {
+  res.status(404).json({
+    status: 'unsuccess',
+    message: 'data is not found',
+  });
+};
